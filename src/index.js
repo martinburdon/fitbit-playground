452a0ce7cce3230ac5e0b6a313e67617d5288bb2
Martin Burdon <martin@martinburdon.co.uk>
2016-09-23 17:31:15 +0100

    More calls

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@
     console.log(userId);
 
     if (authed) {
-      makeRequests({
+      setSleepGoal({
         accessToken,
         userId,
         scope
@@ -39,11 +39,70 @@
     window.open('https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=228229&redirect_uri=http%3A%2F%2Flocalhost%3A8888%2Ffitbit-playground%2Findex.html&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800');
   };
 
-  function makeRequests(data) {
+  function getUserDetails(data) {
     var options = {
       uri: 'https://api.fitbit.com/1/user/-/profile.json',
       headers: {
         Authorization: `Bearer ${data.accessToken}`
+      }
+    };
+
+    request(options, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var data = JSON.parse(body);
+        console.log(data);
+      } else {
+        console.log('error ', error);
+      }
+    });
+  };
+
+  function getHeartRateData(data) {
+    var options = {
+      uri: 'https://api.fitbit.com/1/user/-/activities/heart/date/today/1d.json',
+      headers: {
+        Authorization: `Bearer ${data.accessToken}`
+      }
+    };
+
+    request(options, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var data = JSON.parse(body);
+        console.log(data);
+      } else {
+        console.log('error ', error);
+      }
+    });
+  };
+
+  function getSleepData(data) {
+    var options = {
+      uri: 'https://api.fitbit.com/1/user/-/sleep/date/2016-09-23.json',
+      headers: {
+        Authorization: `Bearer ${data.accessToken}`
+      }
+    };
+
+    request(options, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var data = JSON.parse(body);
+        console.log(data);
+      } else {
+        console.log('error ', error);
+      }
+    });
+  };
+
+  function setSleepGoal(data) {
+    var options = {
+      method: 'POST',
+      uri: 'https://api.fitbit.com/1/user/-/sleep/goal.json',
+      form: {
+        minDuration: '180'
+      },
+      headers: {
+        Authorization: `Bearer ${data.accessToken}`,
+        'WWW-Authenticate': 'Basic realm="api.fitbit.com"'
       }
     };
 
